d9c7e961467d3054f3af76ed802f074a36801f59
Dan Pock <dpock32509@gmail.com>
2018-09-10 22:23:16 -0400

    Add manual composer-less loader..
    ...fix the example index.php file and add a non-composer based loader. This should add an easy route for users to migrate to regardless of their composer usage.

---
+++ src/loader.php
@@ -0,0 +1,35 @@
+<?php
+
+$base_folder = dirname(__DIR__);
+
+$composer = json_decode(file_get_contents("$base_folder/composer.json"), 1);
+$namespaces = $composer['autoload']['psr-4'];
+$filepaths = $composer['autoload']['files'];
+
+foreach ($filepaths as $filepath) {
+    $fullpath = $base_folder.'/'.$filepath;
+    if (file_exists($fullpath)) {
+        include_once $fullpath;
+    }
+}
+
+// Foreach namespace specified in the composer, load the given classes
+foreach ($namespaces as $namespace => $classpaths) {
+    if (!is_array($classpaths)) {
+        $classpaths = array($classpaths);
+    }
+    spl_autoload_register(function ($classname) use ($namespace, $classpaths, $base_folder) {
+        // Check if the namespace matches the class we are looking for
+        if (preg_match("#^".preg_quote($namespace)."#", $classname)) {
+            // Remove the namespace from the file path since it's psr4
+            $classname = str_replace($namespace, "", $classname);
+            $filename = preg_replace("#\\\\#", "/", $classname).".php";
+            foreach ($classpaths as $classpath) {
+                $fullpath = $base_folder."/".$classpath."/$filename";
+                if (file_exists($fullpath)) {
+                    include_once $fullpath;
+                }
+            }
+        }
+    });
+}
